11e50b44e6aa93fb8c1e15e0ea1414af258d9257
Spencer Pearson <speeze.pearson@gmail.com>
2015-05-10 17:39:55 -0700

    added interactive example

---
+++ examples/interactive.py
@@ -0,0 +1,37 @@
+import code
+import threading
+import browsergui
+from browsergui import GUI, Text, run
+
+gui = GUI()
+gui.append(Text("""
+  Run commands in the REPL.
+  As you change `gui`, this page will update.
+  Some commands you might run are:
+"""))
+
+for sample in ("gui.append(Text('Hiiii!'))",
+               "gui.append(Button(callback=(lambda event: gui.append(Text('Clicked!', inline=False)))))"):
+  gui.append(Text(sample, code=True, inline=False))
+
+gui.append(Text("The code for this page is:"))
+gui.append(Text(open(__file__).read(), code=True, inline=False))
+
+t = threading.Thread(target=run, args=(gui,), kwargs=dict(quiet=True))
+t.daemon = True
+t.start()
+
+repl_locals = vars(browsergui).copy()
+repl_locals.update(gui=gui)
+code.interact(
+  banner="""
+    Here's an interpreter! You have access to everything in the `browsergui`
+    namespace, plus a ready-made GUI named `gui`.
+    
+    The server startup might print a couple things on top of the prompt -
+    don't worry, you're still in the interpreter.
+    
+    Exiting the interpreter will terminate the program.
+  """,
+  local=repl_locals)
+gui.destroy()
