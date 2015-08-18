179c0ccf2df9512dc5ee41ff2430016f36c77f21
Spencer Pearson <speeze.pearson@gmail.com>
2015-08-17 17:45:06 -0700

    refactor placeholder into InputField class

--- browsergui/elements/input_field.py
+++ browsergui/elements/input_field.py
@@ -2,7 +2,7 @@ from . import LeafElement
 from ..events import Input
 
 class InputField(LeafElement):
-  def __init__(self, tag_name='input', value=None, change_callback=None, **kwargs):
+  def __init__(self, tag_name='input', value=None, placeholder=None, change_callback=None, **kwargs):
     self.set_cached_value(value)
     super(InputField, self).__init__(tag_name=tag_name, **kwargs)
     self.update_tag_with_cached_value()
@@ -10,6 +10,8 @@ class InputField(LeafElement):
     self.add_callback(Input, lambda event: self.set_cached_value(event.value))
     if change_callback is not None:
       self.add_callback(Input, (lambda event: change_callback()))
+
+    self.placeholder = placeholder
 
   @property
   def value(self):
@@ -20,12 +22,28 @@ class InputField(LeafElement):
     self.handle_event(Input(target_id=self.id, value=self.value))
     self.mark_dirty()
 
+  @property
+  def placeholder(self):
+    return self.tag.getAttribute('placeholder')
+  @placeholder.setter
+  def placeholder(self, placeholder):
+    if placeholder is None:
+      if 'placeholder' in self.tag.attributes.keys():
+        self.tag.removeAttribute('placeholder')
+    else:
+      self.tag.setAttribute('placeholder', placeholder)
+    self.mark_dirty()
+
   def mark_dirty(self):
     self.update_tag_with_cached_value()
     super(InputField, self).mark_dirty()
 
   def update_tag_with_cached_value(self):
-    self.tag.setAttribute('value', self.cached_value)
+    if self.cached_value is None:
+      if 'value' in self.tag.attributes.keys():
+        self.tag.removeAttribute('value')
+    else:
+      self.tag.setAttribute('value', self.cached_value)
 
   def set_cached_value(self, value):
     self.cached_value = value
--- browsergui/elements/textfield.py
+++ browsergui/elements/textfield.py
@@ -1,16 +1,6 @@
 from .input_field import InputField
 
 class TextField(InputField):
-  def __init__(self, placeholder='', **kwargs):
+  def __init__(self, **kwargs):
     super(TextField, self).__init__(**kwargs)
     self.tag.setAttribute('type', 'text')
-
-    self.placeholder = placeholder
-
-  @property
-  def placeholder(self):
-    return self.tag.getAttribute('placeholder')
-  @placeholder.setter
-  def placeholder(self, placeholder):
-    self.tag.setAttribute('placeholder', placeholder)
-    self.mark_dirty()
