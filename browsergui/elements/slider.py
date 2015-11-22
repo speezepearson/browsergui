7b2a51135a0e14e3ac36d8f35766ef9bba90ee0f
Spencer Pearson <suspense@cs.washington.edu>
2015-11-21 17:20:32 -0800

    make ContinuousSlider and IntegerSlider inherit from common ancestor

--- browsergui/elements/slider.py
+++ browsergui/elements/slider.py
@@ -3,40 +3,45 @@ import math
 from .input_field import InputField
 
 class Slider(InputField):
-  '''A continuously draggable slider.
+  '''A draggable slider.
 
-  Useful attributes:
+  Any attempted assignment to ``min``, ``max``, or ``value`` that would violate ``min <= value <= max`` will fail and instead raise a ``ValueError``.
 
-  - ``slider.min`` is the smallest number the slider can be set to. Writable.
-  - ``slider.max`` is the largest number the slider can be set to. Writable.
+  :param min: the smallest value the slider can accept. Writable.
+  :param max: the largest value the slider can accept. Writable.
 
-  ``min``, ``max``, or ``value`` can be set to any real number (e.g. float, int, ``fractions.Fraction``), but any attempted assignment that would violate ``min <= value <= max`` will fail and instead raise a ``ValueError``. When the user drags the slider, the value is set to a float.
+  I think :class:`ContinuousSlider` and :class:`IntegerSlider` are probably the only subclasses you'll ever need, but in case you write your own subclass, it must define:
 
-  :param min: the smallest value the slider can accept
-  :param max: the largest value the slider can accept
+  - ``value_to_xml_string(x)``: convert a value to a string (string should be a number)
+  - ``value_from_xml_string(s)``: inverse of ``value_to_xml_string``
+  - ``_step(min, max)``: step the value should take in the browser
+  - ``_default_value(min, max)``: value to take during initialization if none specified
+  - ``typecheck_value_or_bound(x)``: raise ``TypeError`` if a value isn't suitable for a value or bound
   '''
 
   def __init__(self, min, max, value=None, **kwargs):
     if value is None:
-      value = (min+max)/2
+      value = self._default_value(min, max)
 
     self._min = min
     self._max = max
     super(Slider, self).__init__(value=value, **kwargs)
-    self.tag.setAttribute('type', 'range')
-    self.tag.setAttribute('step', str((self.max-self.min)/1000.))
     self.max = max
     self.min = min
+
+    self.tag.setAttribute('type', 'range')
+    self.tag.setAttribute('step', str(self._step(min, max)))
 
   @property
   def min(self):
     return self._min
   @min.setter
   def min(self, new_min):
+    self.typecheck_value_or_bound(new_min)
     if new_min > self.value:
       raise ValueError("can't set min to above current value")
     self._min = new_min
-    self.tag.setAttribute('min', repr(float(new_min)))
+    self.tag.setAttribute('min', self.value_to_xml_string(new_min))
     self.mark_dirty()
 
   @property
@@ -44,26 +49,80 @@ class Slider(InputField):
     return self._max
   @max.setter
   def max(self, new_max):
+    self.typecheck_value_or_bound(new_max)
     if new_max < self.value:
       raise ValueError("can't set max to below current value")
     self._max = new_max
-    self.tag.setAttribute('max', repr(float(new_max)))
+    self.tag.setAttribute('max', self.value_to_xml_string(new_max))
     self.mark_dirty()
 
-  def value_from_xml_string(self, s):
-    return float(s) if s else None
-
-  def value_to_xml_string(self, x):
-    return '' if x is None else repr(float(x))
-
   def ensure_is_valid_value(self, value):
-    if not isinstance(value, numbers.Real):
-      raise TypeError('expected real-number value, got {}'.format(type(value).__name__))
-
-    if math.isnan(value):
-      raise TypeError('expected real-number value, got NaN')
+    self.typecheck_value_or_bound(value)
 
     if (value < self.min) or (value > self.max):
       raise ValueError('{} not between {} and {}'.format(value, self.min, self.max))
 
     super(Slider, self).ensure_is_valid_value(value)
+
+  @classmethod
+  def _step(cls, min, max):
+    raise NotImplementedError('{} does not implement _step'.format(cls.__name__))
+  @classmethod
+  def _default_value(cls, min, max):
+    raise NotImplementedError('{} does not implement _default_value'.format(cls.__name__))
+  @classmethod
+  def typecheck_value_or_bound(cls, x):
+    raise NotImplementedError('{} does not implement typecheck_value_or_bound'.format(cls.__name__))
+
+class ContinuousSlider(Slider):
+  '''A type of :class:`Slider` that accepts real-valued values/bounds (e.g. float, int, ``fractions.Fraction``).
+
+  When the user drags the slider, the value is set to a float.
+  '''
+
+  @staticmethod
+  def value_from_xml_string(s):
+    return float(s)
+
+  @staticmethod
+  def value_to_xml_string(x):
+    return repr(float(x))
+
+  @staticmethod
+  def _step(min, max):
+    return (max-min) / 1000.
+
+  @staticmethod
+  def _default_value(min, max):
+    return (min+max) / 2.
+
+  @staticmethod
+  def typecheck_value_or_bound(x):
+    if not isinstance(x, numbers.Real):
+      raise TypeError('expected real-number value, got {}'.format(type(x).__name__))
+    if math.isnan(x):
+      raise TypeError('expected real-number value, got NaN')
+
+class IntegerSlider(Slider):
+  '''A type of :class:`Slider` that accepts only integer values/bounds.'''
+
+  @staticmethod
+  def value_from_xml_string(s):
+    return int(s)
+
+  @staticmethod
+  def value_to_xml_string(x):
+    return '{:d}'.format(x)
+
+  @staticmethod
+  def _step(min, max):
+    return 1
+
+  @staticmethod
+  def _default_value(min, max):
+    return (min+max) // 2
+
+  @staticmethod
+  def typecheck_value_or_bound(x):
+    if not isinstance(x, int):
+      raise TypeError('expected int value, got {}'.format(type(x).__name__))
--- test/test_slider.py
+++ test/test_slider.py
@@ -1,10 +1,10 @@
 from fractions import Fraction
-from browsergui import Slider
+from browsergui import ContinuousSlider, IntegerSlider
 from . import BrowserGUITestCase
 
-class SliderTest(BrowserGUITestCase):
+class ContinuousSliderTest(BrowserGUITestCase):
   def test_type_validation(self):
-    s = Slider(min=0, max=10)
+    s = ContinuousSlider(min=0, max=10)
 
     for good_object in [0, 1, 1.1, Fraction(1,10)]:
       s.value = good_object
@@ -14,7 +14,7 @@ class SliderTest(BrowserGUITestCase):
         s.value = bad_object
 
   def test_range_validation(self):
-    s = Slider(min=0, max=5)
+    s = ContinuousSlider(min=0, max=5)
 
     for good_value in [0, 1, 5, 2**0.5]:
       s.value = good_value
@@ -24,7 +24,7 @@ class SliderTest(BrowserGUITestCase):
         s.value = bad_value
 
   def test_bounds(self):
-    s = Slider(min=0, max=5)
+    s = ContinuousSlider(min=0, max=5)
     self.assertEqual(s.min, 0)
     self.assertEqual(s.max, 5)
 
@@ -33,7 +33,7 @@ class SliderTest(BrowserGUITestCase):
     self.assertIsInstance(s.max, int)
 
   def test_set_bounds(self):
-    s = Slider(min=0, max=5)
+    s = ContinuousSlider(min=0, max=5)
     s.value = 3
     s.min = 1
     self.assertEqual(s.min, 1)
@@ -60,13 +60,72 @@ class SliderTest(BrowserGUITestCase):
 
 
   def test_set_bounds__marks_dirty(self):
-    s = Slider(min=0, max=5)
+    s = ContinuousSlider(min=0, max=5)
     with self.assertMarksDirty(s):
       s.min = 1
     with self.assertMarksDirty(s):
       s.max = 4
 
   def test_exotic_bounds(self):
-    s = Slider(min=Fraction(1,10), max=Fraction(9,10))
+    s = ContinuousSlider(min=Fraction(1,10), max=Fraction(9,10))
     self.assertEqual(s.min, Fraction(1, 10))
     self.assertEqual(s.max, Fraction(9, 10))
+
+class IntegerSliderTest(BrowserGUITestCase):
+  def test_type_validation(self):
+    s = IntegerSlider(min=0, max=10)
+
+    for good_object in [0, 1, 10]:
+      s.value = good_object
+
+    for bad_object in [0.0, 1.2, 1j, []]:
+      with self.assertRaises(TypeError):
+        s.value = bad_object
+
+  def test_range_validation(self):
+    s = IntegerSlider(min=0, max=5)
+
+    for good_value in [0, 1, 5]:
+      s.value = good_value
+
+    for bad_value in [-1, 6]:
+      with self.assertRaises(ValueError):
+        s.value = bad_value
+
+  def test_bounds(self):
+    s = IntegerSlider(min=0, max=5)
+    self.assertEqual(s.min, 0)
+    self.assertEqual(s.max, 5)
+
+  def test_set_bounds__marks_dirty(self):
+    s = IntegerSlider(min=0, max=5)
+    with self.assertMarksDirty(s):
+      s.min = 1
+    with self.assertMarksDirty(s):
+      s.max = 4
+
+  def test_set_bounds(self):
+    s = IntegerSlider(min=0, max=5)
+    s.value = 3
+    s.min = 1
+    self.assertEqual(s.min, 1)
+    self.assertEqual(s.value, 3)
+    s.min = 3
+    self.assertEqual(s.min, 3)
+    self.assertEqual(s.value, 3)
+
+    with self.assertRaises(ValueError):
+      s.min = 4
+
+    s.min = 0
+    self.assertEqual(s.min, 0)
+
+    s.max = 4
+    self.assertEqual(s.max, 4)
+    self.assertEqual(s.value, 3)
+    s.max = 3
+    self.assertEqual(s.max, 3)
+    self.assertEqual(s.value, 3)
+
+    with self.assertRaises(ValueError):
+      s.max = 1
